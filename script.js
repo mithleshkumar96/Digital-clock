c986f84ad9299f5b60e9dc3e68e5126ea79c8445
Mithlesh kumar <146717835+mithleshkumar96@users.noreply.github.com>
2023-10-29 01:52:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function clock(){
+    let hours = document.querySelector(".dg-hours")
+    let minutes = document.querySelector(".dg-m")
+    let seconds = document.querySelector(".dg-s")
+    let period = document.querySelector(".dg-p")
+
+let h = new Date().getHours();
+let m = new Date().getMinutes();
+let s = new Date().getSeconds();
+
+let ampm = h >= 12 ? "PM" : "AM";
+
+if (h > 12){
+    h = h - 12;
+}
+
+h = (h < 10) ? "0" + h : h;
+m = (m < 10) ? "0" + m : m;
+s = (s < 10) ? "0" + s : s;
+
+hours.innerHTML = h;
+minutes.innerHTML = m;
+seconds.innerHTML = s;
+period.innerHTML = ampm;
+
+
+
+};
+setInterval(clock,1000);
